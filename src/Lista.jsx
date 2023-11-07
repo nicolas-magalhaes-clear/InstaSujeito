c4312e43c4ae3c6e6167b84476131a760b5d2670
nicolas-magalhaes-clear <nicolas.magalhaes.clear@gmail.com>
2023-11-07 10:04:04 -0300

    Project Complete

--- src/Lista.jsx
+++ src/Lista.jsx
@@ -1,26 +1,73 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 
 
 export default function Lista(props) {
+
+    const initialFeed = props.data
+    const [feed, setFeed] = useState(initialFeed)
+    const [st, setSt] = useState(false)
+
+    function like(){
+
+        let _feed = feed
+
+        if(_feed.likeada === true){
+            
+
+            console.log('Removendo')
+            console.log('Likeada antigo:', _feed.likeada)
+            console.log('Likers antigo:', _feed.likers)
+            _feed.likeada = false
+            _feed.likers = _feed.likers - 1
+            console.log('Likeada NOVO:', _feed.likeada)
+            console.log('Likers NOVO:', _feed.likers)
+
+            setFeed(_feed)
+            setSt(true)
+
+        }
+        else{
+            console.log('Adicionadno')
+            console.log('Likeada antigo:', _feed.likeada)
+            console.log('Likers antigo:', _feed.likers)
+            _feed.likeada = true
+            _feed.likers = _feed.likers + 1
+            console.log('Likeada NOVO:', _feed.likeada)
+            console.log('Likers NOVO:', _feed.likers)
+            setFeed(_feed)
+            setSt(false)
+
+        }
+    }
+
+    function carregaIcone(){
+        if(feed.likeada){
+            return require('./assets/images/likeada.png')
+        }
+        else{
+            return require('./assets/images/like.png')
+        }
+    }
     return (
         <View style={styles.areaFeed}>
             <View style={styles.viewPerfil}>
                 <Image
-                    source={{ uri: props.data.imgperfil }}
+                    source={{ uri: feed.imgperfil }}
                     style={styles.fotoPerfil}
                 />
 
-                <Text style={styles.nomeUsuario}>{props.data.nome}</Text>
+                <Text style={styles.nomeUsuario}>{feed.nome}</Text>
 
             </View>
             <Image
                 style={styles.fotoPublicacao}
-                source={{ uri: props.data.imgPublicacao }}
+                source={{ uri: feed.imgPublicacao }}
             />
             <View style={styles.areaBtn}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={()=> like()}>
                     <Image
-                        source={require('./assets/images/like.png')}
+                        source={carregaIcone()}
                         style={styles.iconeLike}
                     />
                 </TouchableOpacity>
@@ -31,9 +78,10 @@ export default function Lista(props) {
                     />
                 </TouchableOpacity>
             </View>
+            <Text>{feed.likers} {st ? '' : ''}{feed.likers > 1 ? 'curtidas' : 'curtida'}</Text>
             <View style={styles.viewRodape}>
-                <Text style={styles.nomeRodape}>{props.data.nome}</Text>
-                <Text style={styles.descRodape}>{props.data.descricao}</Text>
+                <Text style={styles.nomeRodape}>{feed.nome}</Text>
+                <Text style={styles.descRodape}>{feed.descricao}</Text>
             </View>
         </View>
     )
